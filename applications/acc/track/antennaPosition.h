cb2f9cdf4b065c0768e9d7a89ab681efe46368c9
Nimesh Patel <npatel@cfa.harvard.edu>
2012-06-21 10:52:59 -0400

    Added local.c and mount.c (az/el calculations, pointing model corrections) and Makefile for glttrack. Also added antennaPosition.h file.

---
+++ applications/acc/track/antennaPosition.h
@@ -0,0 +1,10 @@
+/* These position values are for the GLT antenna at the Socorro site
+as measured with a hand-held GPS (android phone). Altitude is from
+wikipedia*/
+#define        LONGITUDE_HOURS -7.174712960
+#define        LATITUDE_DEGREES 34.75083333
+#define        LONGITUDE_DEGREES -107.6206944
+#define        SINLAT 0.570008713
+#define        COSLAT 0.8215638648
+#define        LONGRAD -1.878335461
+#define        HEIGHT_M 2124.0
